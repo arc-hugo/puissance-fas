afc91cce4a6bbcbb046bb581bd13207eab768d95
Hugo Barral <hugo.barral@outlook.fr>
2020-11-30 12:14:39 +0100

    Update Check_Diagonale.js

--- javascript/Check_Diagonale.js
+++ javascript/Check_Diagonale.js
@@ -5,50 +5,29 @@ function Check_Diagonale(y,x,couleur) {
 
 	if (this.Plateau[y-3][x+3] = couleur && this.Plateau[y-2][x+2] = couleur && this.Plateau[y-1][x+1] = couleur &&){ 			//bas droite	
 		win = true;
-		   
-
-	}
-
-	else if (this.Plateau[y+1][x-1] = couleur && this.Plateau[y-2][x+2] = couleur && this.Plateau[y-1][x+1] = couleur &&){		// milieu droite
+	} else if (this.Plateau[y+1][x-1] = couleur && this.Plateau[y-2][x+2] = couleur && this.Plateau[y-1][x+1] = couleur &&){		// milieu droite
 		win = true;
-		 
-	}
-	else if (this.Plateau[y+1][x-1] = couleur && this.Plateau[y-2][x+2] = couleur && this.Plateau[y-1][x+1] = couleur &&){		// milieu gache
+	} else if (this.Plateau[y+1][x-1] = couleur && this.Plateau[y-2][x+2] = couleur && this.Plateau[y-1][x+1] = couleur &&){		// milieu gache
 		win = true;
-		
 	}
 	else if (this.Plateau[y+1][x-1] = couleur && this.Plateau[y+2][x-2] = couleur && this.Plateau[y+3][x-3] = couleur &&){		// haut gauche
 		win = true;
-		 	
 	}
 	
 	//diagonale bas gauche/haut droite
 
-	if (this.Plateau[y+3][x+3] = couleur && this.Plateau[y+2][x+2] = couleur && this.Plateau[y+1][x+1] = couleur &&){    		//bas gauche
-	 			
+	else if (this.Plateau[y+3][x+3] = couleur && this.Plateau[y+2][x+2] = couleur && this.Plateau[y+1][x+1] = couleur &&){    		//bas gauche
 		win = true;
-		  
-
 	}
-	if (this.Plateau[y-1][x-1] = couleur && this.Plateau[y+2][x+2] = couleur && this.Plateau[y+1][x+1] = couleur &&){    		//mileu gauche
-	 			
+	else if (this.Plateau[y-1][x-1] = couleur && this.Plateau[y+2][x+2] = couleur && this.Plateau[y+1][x+1] = couleur &&){    		//mileu gauche
 		win = true;
-		 
-
 	}
-	if (this.Plateau[y-1][x-1] = couleur && this.Plateau[y-2][x-2] = couleur && this.Plateau[y+1][x+1] = couleur &&){    		// mileu droite
-	 			
+	else if (this.Plateau[y-1][x-1] = couleur && this.Plateau[y-2][x-2] = couleur && this.Plateau[y+1][x+1] = couleur &&){    		// mileu droite
 		win = true;
-		   
-
 	}
-	if (this.Plateau[y-1][x-1] = couleur && this.Plateau[y-2][x-2] = couleur && this.Plateau[y-3][x-3] = couleur &&){    		// haut droite
-	 			
+	else if (this.Plateau[y-1][x-1] = couleur && this.Plateau[y-2][x-2] = couleur && this.Plateau[y-3][x-3] = couleur &&){    		// haut droite
 		win = true;
-		   
-
 	}
-
 	else {
 		win =false;
 	}
