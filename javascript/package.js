ad592b5b1409d5e421039687c1d23ff724ab3d88
Hugo Barral <hugo.barral@outlook.fr>
2020-12-09 08:28:43 +0100

    Séparation des classes

--- javascript/package.js
+++ javascript/package.js
@@ -1,24 +1,4 @@
 
-		class Joueur  {
-			constructor(Id,Nom,Score)
-			this.Id = Id;
-			this.Nom = Nom;
-			this.Score = 0;
 
-		}
 
-		class Plateau {
-			constructor(Plateau,Playable_Colonne,size_x,size_y)
-			this.Plateau = Plateau;
-			this.Playable_Colonne = Playable_Colonne;
-			this.size_x = size_x;
-			this.size_y = size_y;
 
-		}
-
-		class Partie {
-			constructor(Current_Player, Board)
-			this.Current_Player = Current_Player;
-			this.Board = Board;
-		}
-	
